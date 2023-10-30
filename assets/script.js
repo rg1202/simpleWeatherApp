9705227887766279ce547528b5a8204aabb2400d
Rob Gould <rgould.midwest@gmail.com>
2023-10-30 18:00:29 -0500

    added/formated current time with day.js, added setinterval to update every second

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
+	const currentDayElement = $("#currentDay");
+	const currentTime = dayjs().format("dddd, MMMM D, YYYY h:mm:ss A"); //format of time
+	const location = document.getElementById("locationInput").value;
+	var map;
+	//modal
+	var elems = document.querySelectorAll(".modal");
+	var instances = M.Modal.init(elems);
 
-
-
-
+	//weather
+	//day and time
+	//display current date and time
+	currentDayElement.text(currentTime);
+	//update time function
+	function updateCurrentTime() {
+		const currentTime = dayjs().format("ddd, MMM D, YYYY h:mm A");
+		currentDayElement.text(currentTime);
+	}
+	// Initial call to set the current date and time
+	updateCurrentTime();
+	// update the current date and time every second
+	setInterval(updateCurrentTime, 1000);
 });
